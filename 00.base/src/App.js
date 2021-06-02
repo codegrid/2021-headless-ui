066e341d45fd4b947433d7e1a8071df887cf83e8
Takeshi Takatsudo <takazudo@gmail.com>
2021-06-02 22:01:03 +0900

    created 00.base

--- 00.base/src/App.js
+++ 00.base/src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <div>Boom! This is an application!</div>;
+  return (
+    <div className="px-8 py-6">
+      <h1 className="text-xl pb-2 font-bold">Headless UI example</h1>
+      <p>The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. </p>
+    </div>
+  );
 }
 
 export default App;
