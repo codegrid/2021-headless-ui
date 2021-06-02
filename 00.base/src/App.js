0a8b0e73037a9829cbc30c7891b00326c9fcf5bd
Takeshi Takatsudo <takazudo@gmail.com>
2021-06-02 23:02:34 +0900

    copied base to simple

--- 00.base/src/App.js
+++ 00.base/src/App.js
@@ -2,7 +2,11 @@ function App() {
   return (
     <div className="px-8 py-6">
       <h1 className="text-xl pb-2 font-bold">Headless UI example</h1>
-      <p>The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. </p>
+      <p>
+        The quick brown fox jumps over the lazy dog. The quick brown fox jumps
+        over the lazy dog. The quick brown fox jumps over the lazy dog. The
+        quick brown fox jumps over the lazy dog.
+      </p>
     </div>
   );
 }
