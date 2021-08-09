a4803f2db42a22a98d349391e97f6484e1b33767
Takeshi Takatsudo <takazudo@gmail.com>
2021-08-09 16:09:50 +0900

    changed import order

--- 03.transition/src/App.js
+++ 03.transition/src/App.js
@@ -1,5 +1,5 @@
-import { Transition } from "@headlessui/react";
 import { useState } from "react";
+import { Transition } from "@headlessui/react";
 
 function App() {
   const [open, setOpen] = useState(false);
